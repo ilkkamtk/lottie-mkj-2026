87a01c610c49b3671dba6c365e50882cd122a384
Ilkka Kylmäniemi <ilkkamtk@km-d557-mim2-01.pk.amk.root>
2026-04-30 10:43:00 +0300

    first commit

---
+++ src/views/Layout.tsx
@@ -0,0 +1,63 @@
+import { Link, Outlet } from "react-router";
+import Lottie from "../components/LottieWrapper";
+import hamburgerAnimation from "../assets/hampurilainen.json";
+import { useState } from "react";
+
+const Layout = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const toggle = () => {
+    setToggleMenu((currentValue) => !currentValue);
+  };
+
+  console.log(toggleMenu);
+
+  return (
+    <div className="m-auto h-full w-11/12">
+      <nav className="absolute right-0 flex flex-col-reverse items-end justify-end lg:relative lg:block lg:flex-row">
+        <ul
+          className={`
+          mr-4
+          justify-end
+          overflow-hidden
+          rounded-lg
+          bg-slate-200
+          p-0
+          shadow-md
+          transition-all
+          duration-500
+          ease-in-out
+          lg:flex
+          lg:opacity-100
+          `}
+        >
+          <li>
+            <Link className="block p-4 text-center  hover:bg-slate-300" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="block p-4 text-center  hover:bg-slate-300"
+              to="/example"
+            >
+              Example
+            </Link>
+          </li>
+        </ul>
+        <div className="m-4 h-8 w-8 sm:block lg:hidden">
+          <Lottie
+            animationData={hamburgerAnimation}
+            loop={false}
+            onClick={toggle}
+          />
+        </div>
+      </nav>
+      <main className="">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
